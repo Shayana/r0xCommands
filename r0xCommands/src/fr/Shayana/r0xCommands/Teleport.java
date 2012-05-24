98cda7831e741fbfabde31bab95079b40a36fd74
CilagINept <galic@voila.fr>
2012-05-24 19:38:57 +0300

    Ajout d'un petit texte de non permission à /tpall

--- r0xCommands/src/fr/Shayana/r0xCommands/Teleport.java
+++ r0xCommands/src/fr/Shayana/r0xCommands/Teleport.java
@@ -200,6 +200,10 @@ public class Teleport {
 				}	
 			}
 		}
+		else {
+			
+			player.sendMessage(ChatColor.RED + "Vous n'avez pas la permission de faire ceci !");
+		}
 	}
 	
 	public void Command_accept() {
